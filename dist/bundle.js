8815d13af14855305af4296c33b3478abe5244a2
Jason Wentworth <jwentworth@coursehero.com>
2018-01-18 16:46:05 -0800

    kingsmoot release

--- dist/bundle.js
+++ dist/bundle.js
@@ -355,7 +355,7 @@ var About = (function (_super) {
     About.prototype.render = function () {
         return (React.createElement("section", { className: "about" },
             React.createElement("h2", null, "Now copy paste decks directly from ThronesDB! Just select and copy the decklist directly from either the edit or view screen"),
-            React.createElement("h3", null, "Latest Full Pack: Journey To Oldtown"),
+            React.createElement("h3", null, "Latest Full Pack: Kingsmoot"),
             React.createElement("p", null, "To use this get your deck in text format and paste into the box on the right. Thronesdb's text download option is this format exactly, though I'm sure other editors can provide a similar format"),
             React.createElement("p", null,
                 "This tool was made to analyze the setup strength of various decks. After importing your deck it will draw random set up hands (right now 5,000 hands), and determine the best(ish) possible setup and gather stats. Right now it's logic is can probably be better. There are settings for determining hand quality and mulligans which can be found on the \"Configure\" tab after importing. The rules this currently uses to determine the ",
